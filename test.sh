4878c958dabeade08d9e55685caab79e39ec0a21
xukang <xukang@chukong-inc.com>
2019-11-08 19:57:31 +0800

    add test

---
+++ test.sh
@@ -0,0 +1,4 @@
+
+curl http://123.56.98.47:8061/api/v1/accounts -H "Content-Type:application/json" -H "authorization:YnVmZW5nQDIwMThidWZlbmc="  -X POST --data '{"account":{"name":"test1","owner_key":"COCOS8J2bZ2r96WKJcePL22975Qh94qrfZ8LsK7mkc44nsoJrDQmCao","referror":"","memo_key":"COCOS51EP3KzxW42gzsmmwMEqvejkHRohKkgZEfpktbWkN4uFDXMs2B","active_key":"COCOS51EP3KzxW42gzsmmwMEqvejkHRohKkgZEfpktbWkN4uFDXMs2B"}}'
+
+echo " "
