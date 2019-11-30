968828dfa67903264d8fa28c9051105a4fc149e3
xukang <xukang@chukong-inc.com>
2019-11-30 18:57:47 +0800

    update config

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ headers = {"content-type": "application/json"}
 #注册帐户的注册人
 # faucet1 1.2.18 faucet2 1.2.19 faucet3 1.2.20 faucet4 1.2.21
 register = "faucet1"
-register_id = 1.2.18 
+register_id = "1.2.18" 
 
 #mysql数据库相关参数
 db = { 
